c73d115ab00b8561070fcdc129117065c407960c
obrightwell <olliebrightwell1@gmail.com>
2024-11-26 13:26:43 +0000

    Added GET handler for health.json

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+const fs = require("fs");
 
 app.use(express.static("MainSite"));
 
@@ -11,5 +12,15 @@ app.listen(port, () => {
 app.get("/", (req, res) => {
   res.sendFile("index.html", (err) => {
     if (err) console.log(err);
+  });
+});
+
+app.get('/health.json', (req, res) => {
+  fs.readFile('./health.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.json(JSON.parse(data));
+    }
   });
 });
