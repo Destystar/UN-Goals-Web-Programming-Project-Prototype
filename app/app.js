0e9067bda6aef99606346c82e1bde50d6f38186f
Destystar <95242511+Destystar@users.noreply.github.com>
2024-11-27 17:29:46 +0000

    1. made the "public" folder and named it External Files 2. added CSS to home page 3. modified app.js to utilise the public folder using express

--- app/app.js
+++ app/app.js
@@ -1,13 +1,18 @@
 var express = require("express");
 var app = express();
+var port = 8080;
 
 //set the view engine to ejs
 app.set("view engine", "ejs");
+
+//set the public folder
+app.use(express.static("External Files"));
 
 //index page
 app.get("/", function (req, res) {
   res.render("index");
 });
 
-app.listen(8080);
-console.log("Server is listening on port 8080");
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}`);
+});
