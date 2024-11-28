4e74be57c8502fff93894c7154a65d5aec1b1f7b
Destystar <95242511+Destystar@users.noreply.github.com>
2024-11-28 18:57:42 +0000

    1. fixed redirects from other pages 2. added other pages directories to ejs rendering

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var nodemailer = require("nodemailer");
 var app = express();
 var port = 8080;
 
@@ -8,9 +9,44 @@ app.set("view engine", "ejs");
 //set the public folder
 app.use(express.static("External Files"));
 
-//index page
+//initial page
 app.get("/", function (req, res) {
   res.render("index");
+});
+
+//renders home page
+app.get("/index", function (req, res) {
+  res.render("index");
+});
+
+//renders team page
+app.get("/team", function (req, res) {
+  res.render("team");
+});
+
+//renders signup page
+app.get("/signup", function (req, res) {
+  res.render("signup");
+});
+
+//renders goal select page
+app.get("/goal%20select", function (req, res) {
+  res.render("goal select");
+});
+
+//renders health page
+app.get("/health", function (req, res) {
+  res.render("health");
+});
+
+//renders energy page
+app.get("/energy", function (req, res) {
+  res.render("energy");
+});
+
+//renders land life page
+app.get("/land%20life", function (req, res) {
+  res.render("land life");
 });
 
 app.listen(port, () => {
