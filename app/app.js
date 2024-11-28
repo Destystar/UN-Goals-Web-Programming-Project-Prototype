6b43d33545f5b3d85ab5a3f6455d142bbeacf30f
Ben <ben.a.maddison@gmail.com>
2024-11-28 23:02:33 +0000

    Fixed Cannot GET error on goal select page

--- app/app.js
+++ app/app.js
@@ -13,50 +13,6 @@ app.set('views', path.join(__dirname, 'views'));
 //set the public folder
 app.use(express.static("public"));
 
-<<<<<<< HEAD
-//initial page
-app.get("/", function (req, res) {
-  res.render("index");
-});
-
-//renders home page
-app.get("/index", function (req, res) {
-  res.render("index");
-});
-
-//renders team page
-app.get("/team", function (req, res) {
-  res.render("team");
-});
-
-//renders signup page
-app.get("/signup", function (req, res) {
-  res.render("signup");
-});
-
-//renders goal select page
-app.get("/goal%20select", function (req, res) {
-  res.render("goal select");
-});
-
-//renders health page
-app.get("/health", function (req, res) {
-  res.render("health");
-});
-
-//renders energy page
-app.get("/energy", function (req, res) {
-  res.render("energy");
-});
-
-//renders land life page
-app.get("/land%20life", function (req, res) {
-  res.render("land life");
-});
-
-app.listen(port, () => {
-  console.log(`server is listening on port ${port}`);
-=======
 //index page
 app.get("/", (req, res) => {
   res.render("index");
@@ -65,7 +21,6 @@ app.get("/", (req, res) => {
 // energy page
 app.get("/energy", (req, res) => {
   res.render("energy");
->>>>>>> 3c085746d34fb92a74cddbbd248ac40cfb824a03
 });
 
 // goal select
@@ -98,11 +53,11 @@ app.listen(port, () => {
   console.log("Connection Successful");
 });
 
-app.get("/", (req, res) => {
+/*app.get("/", (req, res) => {
   res.sendFile("index.html", (err) => {
     if (err) console.log(err);
   });
-});
+});*/
 
 app.get('/health.json', (req, res) => {
   fs.readFile('./health.json', 'utf8', (err, data) => {
