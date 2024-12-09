730a32401a2d3771223ce25b7c0c8bab529eff32
Destystar <95242511+Destystar@users.noreply.github.com>
2024-12-09 11:23:14 +0000

    adding un logo and formatting home page image not loading when dynamically loadingS

--- app/public/JavaScript/index.js
+++ app/public/JavaScript/index.js
@@ -1,34 +1,42 @@
-const filePath = "Data/index.json"
+const filePath = "Data/index.json";
 
-const titleElement = document.querySelector("title")
-const headerElement = document.querySelector("#header")
-const groupInfoElement = document.querySelector("#Group_info")
-const summaryElement = document.querySelector("#Summary")
+const titleElement = document.querySelector("title");
+const headerElement = document.querySelector("#header");
+const groupInfoElement = document.querySelector("#Group_info");
+const summaryElement = document.querySelector("#Summary");
+const centerContent = document.querySelector("#Center_content");
+const imageDiv = document.querySelector("Image");
 
 document.addEventListener("DOMContentLoaded", () => {
-    fetch(filePath)
-        .then(response => response.json())
-        .then(responseData => {
-            titleElement.innerText = responseData["title"]
-            headerElement.innerText = responseData["header"]
+  fetch(filePath)
+    .then((response) => response.json())
+    .then((responseData) => {
+      titleElement.innerText = responseData["title"];
+      headerElement.innerText = responseData["header"];
 
-            let header2Element = document.createElement("h3")
-            header2Element.innerText = responseData["header2"]
+      let header2Element = document.createElement("h3");
+      header2Element.innerText = responseData["header2"];
 
-            let memberlistElement = document.createElement("ul")
-            memberlistElement.setAttribute("id", "Name_list")
+      let memberlistElement = document.createElement("ul");
+      memberlistElement.setAttribute("id", "Name_list");
 
-            for (i of responseData["member-list"]) {
-                let memberElement = document.createElement("li")
-                memberElement.innerText = i
-                memberlistElement.appendChild(memberElement)
-            }
+      for (i of responseData["member-list"]) {
+        let memberElement = document.createElement("li");
+        memberElement.innerText = i;
+        memberlistElement.appendChild(memberElement);
+      }
 
-            let pElement = document.createElement("p")
-            pElement.innerText = responseData["summary"]
+      let logoElement = document.createElement("img");
+      imgElement.setAttribute("id", "Logo");
+      imgElement.setAttribute("src", responseData["logo"]);
+      imgElement.setAttribute("alt", responseData["alt"]);
 
-            groupInfoElement.appendChild(header2Element)
-            groupInfoElement.appendChild(memberlistElement)
-            summaryElement.appendChild(pElement)
-        })
-})
+      let pElement = document.createElement("p");
+      pElement.innerText = responseData["summary"];
+
+      groupInfoElement.appendChild(header2Element);
+      groupInfoElement.appendChild(memberlistElement);
+      imageDiv.appendChild(logoElement);
+      summaryElement.appendChild(pElement);
+    });
+});
