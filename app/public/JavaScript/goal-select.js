2aa574e78e7ad413ffede26c83515e821409fe8c
Destystar <95242511+Destystar@users.noreply.github.com>
2024-12-10 12:51:42 +0000

    fixed alt text and active navbar colour

--- app/public/JavaScript/goal-select.js
+++ app/public/JavaScript/goal-select.js
@@ -1,39 +1,42 @@
-const filePath = "Data/goal-select.json"
+const filePath = "Data/goal-select.json";
 
-const titleElement = document.querySelector("title")
-const imagesElement = document.querySelector("#Images")
+const titleElement = document.querySelector("title");
+const imagesElement = document.querySelector("#Images");
 
 document.addEventListener("DOMContentLoaded", () => {
-    fetch(filePath)
-        .then(response => response.json())
-        .then(responseData => {
-            titleElement.innerText = responseData["title"]
+  fetch(filePath)
+    .then((response) => response.json())
+    .then((responseData) => {
+      titleElement.innerText = responseData["title"];
 
-            let containerElement = document.createElement("div")
-            containerElement.setAttribute("class", "container")
+      let containerElement = document.createElement("div");
+      containerElement.setAttribute("class", "container");
 
-            let healthAnchor = document.createElement("a")
-            healthAnchor.setAttribute("href", "health")
-            let healthImage = document.createElement("img")
-            healthImage.setAttribute("src", responseData["image"])
-            healthAnchor.appendChild(healthImage)
+      let healthAnchor = document.createElement("a");
+      healthAnchor.setAttribute("href", "health");
+      let healthImage = document.createElement("img");
+      healthImage.setAttribute("src", responseData["image"]);
+      img2Element.setAttribute("alt", responseData["alt"]);
+      healthAnchor.appendChild(healthImage);
 
-            let energyAnchor = document.createElement("a")
-            energyAnchor.setAttribute("href", "energy")
-            let energyImage = document.createElement("img")
-            energyImage.setAttribute("src", responseData["image2"])
-            energyAnchor.appendChild(energyImage)
+      let energyAnchor = document.createElement("a");
+      energyAnchor.setAttribute("href", "energy");
+      let energyImage = document.createElement("img");
+      energyImage.setAttribute("src", responseData["image2"]);
+      img2Element.setAttribute("alt", responseData["alt2"]);
+      energyAnchor.appendChild(energyImage);
 
-            let landlifeAnchor = document.createElement("a")
-            landlifeAnchor.setAttribute("href", "land-life")
-            let landlifeImage = document.createElement("img")
-            landlifeImage.setAttribute("src", responseData["image3"])
-            landlifeAnchor.appendChild(landlifeImage)
+      let landlifeAnchor = document.createElement("a");
+      landlifeAnchor.setAttribute("href", "land-life");
+      let landlifeImage = document.createElement("img");
+      landlifeImage.setAttribute("src", responseData["image3"]);
+      img2Element.setAttribute("alt", responseData["alt3"]);
+      landlifeAnchor.appendChild(landlifeImage);
 
-            containerElement.appendChild(healthAnchor)
-            containerElement.appendChild(energyAnchor)
-            containerElement.appendChild(landlifeAnchor)
+      containerElement.appendChild(healthAnchor);
+      containerElement.appendChild(energyAnchor);
+      containerElement.appendChild(landlifeAnchor);
 
-            imagesElement.appendChild(containerElement)
-        })
-})
+      imagesElement.appendChild(containerElement);
+    });
+});
